f9fd0e59a0c63e0459a593c022385298b599d452
hmmasudsarder <hmmasudsardar@gmail.com>
2026-06-27 00:28:42 +0600

    added uery for sort and filter

--- src/app/modules/preorder/preorder.service.ts
+++ src/app/modules/preorder/preorder.service.ts
@@ -1,3 +1,4 @@
+import QueryBuilder from "../../builder/QueryBuilder";
 import AppError from "../../errors/AppError";
 import { httpStatus } from "../../utils/httpStatus";
 import prisma from "../../utils/prisma";
@@ -11,8 +12,25 @@ const createpreorder = async (data: any) => {
 };
 
 const getAllpreorders = async (query: Record<string, any>) => {
-    const result = await prisma.preorder.findMany();
-    return result;
+  // ১. Express কুয়েরি থেকে স্ট্রিং হিসেবে আসা "true"/"false" কে বুুলিয়ানে রূপান্তর করা
+  const processedQuery = { ...query };
+  if (processedQuery.status === "true") processedQuery.status = true;
+  if (processedQuery.status === "false") processedQuery.status = false;
+
+  // ২. আপনার ক্যোয়ারী বিল্ডার চেইনিং
+  const preOrderData = new QueryBuilder(prisma.preorder, processedQuery)
+    .filter()    // Active, Inactive ফিল্টারের জন্য
+    .sort()      // Newest/Oldest/Products সর্টিং এর জন্য
+    .paginate(); // পেজিনেশনের জন্য
+
+  // ৩. এক্সিকিউশন ও মেটাডাটা (টোটাল কাউন্ট) রিটার্ন
+  const result = await preOrderData.execute();
+  const meta = await preOrderData.countTotal();
+
+  return {
+    meta, // এর ভেতর page, limit, total, totalPage থাকবে যা ফ্রন্টএন্ড পেজিনেশনে লাগবে
+    result,
+  };
 };
 
 const getSinglepreorder = async (id: string) => {
